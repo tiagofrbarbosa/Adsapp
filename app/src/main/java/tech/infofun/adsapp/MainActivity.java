4dcb3de9f65cd90b43afe1caca39b8f0c548aac1
Tiago Barbosa <tfreirebar@gmail.com>
2017-05-21 17:26:14 -0300

    Feat: Launch add app

---
+++ app/src/main/java/tech/infofun/adsapp/MainActivity.java
@@ -0,0 +1,16 @@
+package tech.infofun.adsapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by tfbarbosa on 21/05/17.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
